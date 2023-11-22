6a8987f47b2231d4676610e0a02b788a66a617d2
haroldhenao0 <haroldhenao0@gmail.com>
2023-11-22 14:03:20 -0500

    4 commit

--- src/app/recoger/recoger.page.ts
+++ src/app/recoger/recoger.page.ts
@@ -1,39 +1,75 @@
 import { Component, OnInit } from '@angular/core';
 import { ServicioService } from '../service/servicio.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recoger',
   templateUrl: './recoger.page.html',
   styleUrls: ['./recoger.page.scss'],
 })
-export class RecogerPage  {
-  carril: string="";
-usuarioUid: any= this.firebaseService.usuarioActual.uid;;
-  
+export class RecogerPage {
+  carril: string = "";
+  usuarioUid: any;
 
-  constructor(private firebaseService:ServicioService) { }
+  constructor(private firebaseService: ServicioService, private alertController: AlertController) { }
 
- // ngOnInit() {
-  //}
-
-  registrarRecogida() {
-    const usuario = this.firebaseService.usuarioActual.uid;
-    this.firebaseService.registrarRecogida(this.carril, usuario)
-      .then(() => {
-        console.log('Recogida registrada con éxito');
-      })
-      .catch((error) => {
-        console.error('Error al registrar recogida:', error);
-      });
+  ngOnInit() {
+    // Verifica si usuarioActual está definido en el servicio
+    if (this.firebaseService.usuarioActual) {
+      this.usuarioUid = this.firebaseService.usuarioActual.uid;
+    } else {
+      // Puedes manejar el caso donde usuarioActual no está definido, por ejemplo, redirigir a la página de inicio de sesión.
+      console.error('El objeto usuarioActual no está definido.');
+    }
   }
 
-  eliminarRecogida(id: string) {
-    this.firebaseService.eliminarRecogida(id)
-      .then(() => {
-        console.log('Recogida eliminada con éxito');
-      })
-      .catch((error) => {
-        console.error('Error al eliminar recogida:', error);
-      });
+  async registrarRecogida() {
+    const usuario = this.firebaseService.usuarioActual.uid;
+
+    try {
+      await this.firebaseService.registrarRecogida(this.carril, usuario);
+      console.log('Recogida registrada con éxito');
+      await this.presentSuccessAlert('Recogida registrada con éxito');
+    } catch (error) {
+      console.error('Error al registrar recogida:', error);
+      await this.presentErrorAlert('Error al registrar la recogida');
+    }
+  }
+
+  async eliminarRecogida(id: string) {
+    try {
+      // Elimina la recogida de la base de datos
+      await this.firebaseService.eliminarRecogida(id);
+      console.log('Recogida eliminada con éxito');
+      
+      // Muestra una alerta de éxito
+      await this.presentSuccessAlert('Recogida eliminada con éxito');
+    } catch (error) {
+      console.error('Error al eliminar recogida:', error);
+      
+      // Muestra una alerta de error
+      await this.presentErrorAlert('Error al eliminar la recogida');
+    }
+  }
+
+  async presentSuccessAlert(message: string) {
+    const alert = await this.alertController.create({
+      header: 'Éxito',
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async presentErrorAlert(message: string) {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 }
+
--- src/app/service/servicio.service.ts
+++ src/app/service/servicio.service.ts
@@ -7,69 +7,71 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 })
 export class ServicioService {
 
-  usuarioActual: any
+  usuarioActual: any; // Usuario actualmente autenticado
 
   constructor(
     private afAuth: AngularFireAuth,
     private firestore: AngularFirestore
   ) { 
-
+    // Suscribirse al cambio de estado de autenticación
     this.afAuth.authState.subscribe((usuario) => {
-      this.usuarioActual = usuario;
+      this.usuarioActual = usuario; // Almacena el usuario actual
     });
   }
 
+  // Método para registrarse
   async signUp(nombre: string, cedula: string, celular: string, correo: string, password: string): Promise<void> {
     const userCredential = await this.afAuth.createUserWithEmailAndPassword(cedula, password);
     
- if (userCredential.user) {
-    const user = userCredential.user;
+    if (userCredential.user) {
+      const user = userCredential.user;
 
-    // Almacena información adicional del usuario en Firestore
-    await this.firestore.collection('usuarios').doc(user.uid).set({
-      nombre,
-      cedula,
-      celular,
-      correo
-    });
-  } else {
-    // Manejar el caso donde user es null, si es necesario
-    console.error('El objeto user es nulo.');
-  }
+      // Almacena información adicional del usuario en Firestore
+      await this.firestore.collection('usuarios').doc(user.uid).set({
+        nombre,
+        cedula,
+        celular,
+        correo
+      });
+    } else {
+      // Manejar el caso donde user es null, si es necesario
+      console.error('El objeto user es nulo.');
+    }
   }
 
+  // Método para iniciar sesión
   async signIn(email: string, password: string): Promise<void> {
     await this.afAuth.signInWithEmailAndPassword(email, password);
   }
 
+  // Método para cerrar sesión
   signOut(): Promise<void> {
     return this.afAuth.signOut();
   }
 
-
+  // Método para registrar un alumno
   registrarAlumno(nombre: string, seccion: string, usuarioUid: string) {
     return this.firestore.collection('alumnos').add({ nombre, seccion, usuarioUid });
   }
   
-    // Obtener alumnos de Firebase
-obtenerAlumnosPorUsuario(usuarioUid: string) {
-  return this.firestore.collection('alumnos', ref => ref.where('usuarioUid', '==', usuarioUid)).snapshotChanges();
+  // Método para obtener alumnos por usuario
+  obtenerAlumnosPorUsuario(usuarioUid: string) {
+    return this.firestore.collection('alumnos', ref => ref.where('usuarioUid', '==', usuarioUid)).snapshotChanges();
+  }
+  
+  // Método para eliminar un alumno
+  eliminarAlumno(id: string) {
+    return this.firestore.collection('alumnos').doc(id).delete();
+  }
+  
+  // Método para registrar una recogida
+  registrarRecogida(carril: string, alumnoId: string) {
+    return this.firestore.collection('recogidas').add({ carril, alumnoId });
+  }
+  
+  // Método para eliminar una recogida
+  eliminarRecogida(id: string) {
+    return this.firestore.collection('recogidas').doc(id).delete();
+  }
 }
-  
-    // Eliminar alumno de Firebase
-    eliminarAlumno(id: string) {
-      return this.firestore.collection('alumnos').doc(id).delete();
-    }
-  
-    // Registrar recogida en Firebase
-    registrarRecogida(carril: string, alumnoId: string) {
-      return this.firestore.collection('recogidas').add({ carril, alumnoId });
-    }
-  
-    // Eliminar recogida de Firebase
-    eliminarRecogida(id: string) {
-      return this.firestore.collection('recogidas').doc(id).delete();
-    }
-  
 
-}
